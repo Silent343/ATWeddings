259aba95b836b722a2704d0a7e860d8ccc2f7fc6
Silent343 <gabrielfgs2004@gmail.com>
2022-11-21 22:39:27 -0500

    V 0.2.5

--- 2ndpag/cnt.js
+++ 2ndpag/cnt.js
@@ -4,11 +4,3 @@ const navToggle = document.querySelector(".menuToggle")
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("blockft")
 }) 
-
-
-ScrollReveal().reveal()
-window.sr = ScrollReveal();
-    sr.reveal('.rvlimg', {
-    duration: 4000,
-    reset: true,
-    });
